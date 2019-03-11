6a814781e044efd66494701ef54492ccd98a3a2e
qflbai <qflbai@163.com>
2019-03-11 15:31:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+本作品为参加Hencoder投稿比赛之作
+HenCoder「仿写酷界面」活动——征稿 http://hencoder.com/activity-mock-1
+### 即刻点赞
+
+![](images/like.gif)
+
+### 薄荷健康尺
+
+![](images/ruler.gif)
+
+### 小米运动
+
+![](images/miMove.gif)
+
+### Fliboard 翻页效果
+
+![](images/flipboard.gif)
---
+++ app/src/main/java/com/qflbai/view/like/LikeLayout.java
@@ -0,0 +1,47 @@
+package com.qflbai.view.like;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import com.qflbai.view.R;
+
+
+public class LikeLayout extends RelativeLayout {
+    LikeView view;
+    Button animateBt;
+    private EditText etNum;
+
+    public LikeLayout(Context context) {
+        super(context);
+    }
+
+    public LikeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LikeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        view = (LikeView) findViewById(R.id.objectAnimatorView);
+        etNum = (EditText) findViewById(R.id.editNum);
+        animateBt = (Button) findViewById(R.id.animateBt);
+
+        OnClickListener listener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.setDrawNum(Integer.parseInt(etNum.getText().toString()));
+                view.clear();
+            }
+        };
+        animateBt.setOnClickListener(listener);
+    }
+}
